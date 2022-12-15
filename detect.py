1272ddcde4316cbd9e139e3f53a80b6724da4b7e
dennis <dennis.lofqvist@knowit.se>
2022-12-15 09:38:17 +0100

    added init

--- detect.py
+++ detect.py
@@ -5,7 +5,6 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
-#sfsdfs
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
@@ -15,26 +14,26 @@ from utils.torch_utils import select_device, load_classifier, time_synchronized,
 
 
 '''
-parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
-    opt = parser.parse_args()
+    Arguments
+    ('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    ('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    ('--img-size', type=int, default=640, help='inference size (pixels)')
+    ('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    ('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    ('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    ('--view-img', action='store_true', help='display results')
+    ('--save-txt', action='store_true', help='save results to *.txt')
+    ('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    ('--save-crop', action='store_true', help='save cropped prediction boxes')
+    ('--nosave', action='store_true', help='do not save images/videos')
+    ('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    ('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    ('--augment', action='store_true', help='augmented inference')
+    ('--update', action='store_true', help='update all models')
+    ('--project', default='runs/detect', help='save results to project/name')
+    ('--name', default='exp', help='save results to project/name')
+    ('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    ('--no-trace', action='store_true', help='don`t trace model')
 '''
 def detect(source, weights, conf_thres, save_crop):
     weights = weights
--- kanelbulle.py
+++
@@ -1,3 +0,0 @@
-import detect
-
-imdetect = detect.detect("/home/dennis/repos/yolov7-modified/5566703657_2021-12_13.jpg", "/home/dennis/repos/yolov7-modified/best.pt", 0.5, True)
