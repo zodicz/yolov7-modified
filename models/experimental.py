8b9d3f93b9ff2f15fd6727170880522baa751321
dennis <dennis.lofqvist@knowit.se>
2023-01-09 10:32:21 +0100

    faulty import

--- models/experimental.py
+++ models/experimental.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 from common import Conv, DWConv
-from yolo.utils.google_utils import attempt_download
+from utils.google_utils import attempt_download
 
 
 class CrossConv(nn.Module):
